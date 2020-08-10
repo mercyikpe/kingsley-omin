ff4071a913293a5034ed920c4bfc77856d672094
smallbusiness <ikpemercy1@yahoo.com>
2020-08-10 09:03:21 +0100

    setup files added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+		<Nav class="bg-green-light" />
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -30,3 +31,9 @@
   color: #42b983;
 }
 </style>
+<script>
+	import Nav from "./components/Nav";
+	export default {
+		components: {Nav}
+	}
+</script>
